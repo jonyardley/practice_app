d1db90a46964f5d3275f91d1ec15dacc414dc24b
Jon Yardley <jonyardley@Jons-MacBook-Air.local>
2024-10-19 18:44:19 +0100

    Initial commit

---
+++ shared_types/build.rs
@@ -0,0 +1,17 @@
+use crux_core::typegen::TypeGen;
+use shared::App;
+use std::path::PathBuf;
+
+fn main() -> anyhow::Result<()> {
+    println!("cargo:rerun-if-changed=../shared");
+
+    let mut gen = TypeGen::new();
+
+    gen.register_app::<App>()?;
+
+    let output_root = PathBuf::from("./generated");
+
+    gen.swift("SharedTypes", output_root.join("swift"))?;
+
+    Ok(())
+}
