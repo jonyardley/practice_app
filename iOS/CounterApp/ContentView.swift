45bb2419f150ba8c042edd94f8dc34e5415e0e5b
Jon Yardley <jonyardley@me.com>
2024-10-20 15:18:15 +0100

    Added major and minor scales

--- iOS/CounterApp/ContentView.swift
+++ iOS/CounterApp/ContentView.swift
@@ -27,8 +27,61 @@ struct ContentView: View {
     }
 
     struct ExercisesView: View {
+        let majorScales: [String] = [
+            "C Major", "G Major", "D Major", "A Major", "E Major", "B Major",
+            "F# Major / Gb Major", "C# Major / Db Major", "G# Major / Ab Major",
+            "D# Major / Eb Major", "A# Major / Bb Major", "F Major",
+        ]
+
+        let minorScales: [String] = [
+            "A Minor", "E Minor", "B Minor", "F# Minor / Gb Minor", "C# Minor / Db Minor",
+            "G# Minor / Ab Minor",
+            "D# Minor / Eb Minor", "A# Minor / Bb Minor", "F Minor", "C Minor", "G Minor",
+            "D Minor",
+        ]
+
         var body: some View {
-            Text("Exercises")
+            VStack(alignment: .leading) {
+                Text("Exercises")
+                    .font(.largeTitle)
+                    .padding(.bottom, 10)
+                    .padding(.leading)
+
+                List {
+                    Section(header: Text("Major Scales")) {
+                        ForEach(majorScales, id: \.self) { scale in
+                            HStack {
+                                Text(scale)
+                                Spacer()
+                                CheckBoxView()
+                            }
+                        }
+                    }
+
+                    Section(header: Text("Minor Scales")) {
+                        ForEach(minorScales, id: \.self) { scale in
+                            HStack {
+                                Text(scale)
+                                Spacer()
+                                CheckBoxView()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    struct CheckBoxView: View {
+        @State private var isChecked: Bool = false
+
+        var body: some View {
+            Button(action: {
+                isChecked.toggle()
+            }) {
+                Image(systemName: isChecked ? "checkmark.square" : "square")
+                    .foregroundColor(isChecked ? .blue : .gray)
+            }
         }
     }
 
